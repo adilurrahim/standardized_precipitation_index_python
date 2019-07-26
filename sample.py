42d964c6af0eb77d45d5b3f0d8db1ded49ee2ca9
adilurrahim <adilurrahim@gmail.com>
2019-07-26 22:38:10 +0600

    Delete sample.py

--- sample.py
+++
@@ -1,9 +0,0 @@
-'data_type is weekly with no null value'
-data_for= 30   # the number of week for which spi will be calculated
-time_scale= 11 # = cumulative weeks
-path= 'D:/'   # path to save the result
-
-df= pd.read_csv('Barishal.csv')
-spi= spi_submonthly(df,data_for,time_scale,path)
-spi.columns=['SPI']
-spi.to_csv('spi.csv',index=False)
